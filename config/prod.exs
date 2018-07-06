6296c97d4a2c60387cba2293dbf8679d88643b94
ulm <ulm@zivitech.com>
2018-07-06 11:03:59 +0300

    Enable dummy provider on prod

--- config/prod.exs
+++ config/prod.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
 config :accent, Accent.Endpoint, check_origin: false
-config :accent, dummy_provider_enabled: false
+config :accent, dummy_provider_enabled: true
 
 config :logger, level: :info
